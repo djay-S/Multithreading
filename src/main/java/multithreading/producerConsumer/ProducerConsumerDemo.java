e800655ae3a769bff3ac52fe18e46a1298c9e5a3
djay-S <noemail@github.com>
2022-09-19 21:05:09 +0530

    Producer-Consumer: https://www.youtube.com/watch?v=Vrt5LqpH2D0&list=PLBB24CFB073F1048E&index=7

---
+++ src/main/java/multithreading/producerConsumer/ProducerConsumerDemo.java
@@ -0,0 +1,67 @@
+package multithreading.producerConsumer;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+//https://www.youtube.com/watch?v=Vrt5LqpH2D0&list=PLBB24CFB073F1048E&index=7
+public class ProducerConsumerDemo {
+
+//    No need to explicitly synchromise as it is taken care of by the BlockingQueue
+//    If the queue is full, BlockingQueue will wait till consumes the queue
+//    If the queue is empty, BlockingQueue will wait till producer adds to the queue
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+    private static Random random = new Random();
+
+
+    //    Producers are responsible for producing things which can be shared across other producers using the queue
+    public static void producer() throws InterruptedException {
+
+        while (true) {
+            queue.put(random.nextInt(100));
+        }
+    }
+
+//    Consumers are responsible for consuming the things from the queue
+    public static void consumer() throws InterruptedException {
+
+        while (true) {
+            Thread.sleep(100);
+            if (random.nextInt(10) == 0) {
+                Integer takenValue = queue.take();
+
+                System.out.printf("Taken value: %d; Queue size is: %d%n", takenValue, queue.size());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread producerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    producer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread consumerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        producerThread.start();
+        consumerThread.start();
+
+        producerThread.join();
+        consumerThread.join();
+    }
+}
